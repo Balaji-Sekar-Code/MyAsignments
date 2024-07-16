ce553119ecc44fb5e054524a8c3799e920ceab32
Balaji-Sekar-Code <ssb121998@gmail.com>
2024-07-16 21:19:09 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,18 @@
+package week1.Day2;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2004;
+		char shortcutKey = 'c';
+		System.out.println("verion:"+version+"\n"+"developer:"+developer+"\n"+
+		"isBeta"+isBeta+"\n"+"releaseYear:"+releaseYear+"\n"+
+				"shortcutKey:"+shortcutKey);
+
+	}
+
+}
